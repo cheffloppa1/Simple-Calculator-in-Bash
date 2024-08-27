a329fedf3a27b66cae8c80629a5a5f70cb66e164
cheffloppa1 <chefseller8@gmail.com>
2024-08-27 14:37:59 +0300

    calc.sh

---
+++ calc.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+read -p "Enter a number: " NUMBER1
+read -p "Enter a second number: " NUMBER2
+
+echo "The sum of $NUMBER1 and $NUMBER2 is equal to $(($NUMBER1+$NUMBER2))"
+echo "The difference of $NUMBER1 and $NUMBER2 is equal to $(($NUMBER1-$NUMBER2))"
